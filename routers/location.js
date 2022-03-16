887568ccbcf155bc7f04fb70a72ebd0ff212b78e
ET45 <emre.tugutlu@gmail.com>
2022-03-16 16:42:03 +0300

    second endpoint

--- routers/location.js
+++ routers/location.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const Location = require("../models/").location;
+const Comment = require("../models/").comment;
+const User = require("../models/").user;
 const router = new Router();
 
 router.get("/locations", async (req, res) => {
@@ -8,6 +10,18 @@ router.get("/locations", async (req, res) => {
     res.status(200).send(allLocation);
   } catch (e) {
     console.log("error", e);
+  }
+});
+
+router.get("/location/:id", async (req, res) => {
+  try {
+    const locationId = parseInt(req.params.id);
+    const location = await Location.findByPk(locationId, {
+      include: [Comment, User],
+    });
+    res.status(200).send(location);
+  } catch (e) {
+    console.log("e", e);
   }
 });
 
