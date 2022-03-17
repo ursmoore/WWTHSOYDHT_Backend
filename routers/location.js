563025e41c2d00998c5c6d5218580776f6268b90
ET45 <emre.tugutlu@gmail.com>
2022-03-17 14:29:57 +0300

    patch for dislike

--- routers/location.js
+++ routers/location.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
+const req = require("express/lib/request");
 const Location = require("../models/").location;
 const Comment = require("../models/").comment;
 const User = require("../models/").user;
+const authMiddleware = require("../auth/middleware");
 const router = new Router();
 
 router.get("/locations", async (req, res) => {
@@ -25,4 +27,38 @@ router.get("/location/:id", async (req, res) => {
   }
 });
 
+//SECOND ID WITHOUT COMMENT
+
+/* router.get("/location/:id", async (req, res) => {
+  const locationId = parseInt(req.params.id);
+  const location = await Location.findByPk(locationId, { include: User });
+  res.status(200).send(location);
+});
+ */
+
+router.patch("/location/:id", async (req, res) => {
+  const location = await Location.findByPk(req.params.id);
+  const { dislikes } = req.body;
+  await location.update({ dislikes });
+  return res.status(200).send({ dislikes });
+});
+
+/* router.post("/locations", authMiddleware, async (req, res) => {
+  const { name, image, description, latitude, longtitude, experience } =
+    req.body;
+  const userId = req.user.id;
+
+  const newPost = await Location.create({
+    userId: userId,
+    name,
+    image,
+    description,
+    latitude,
+    longtitude,
+    experience,
+  });
+
+  return res.status(200).send({ newPost });
+});
+ */
 module.exports = router;
